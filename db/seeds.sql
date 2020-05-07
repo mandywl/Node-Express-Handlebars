08e341500a0ddb3070215dbfa483eac861193d81
mandywl <Mengsi.Wells-Lakeland@vu.edu.au>
2020-05-08 00:58:32 +1000

    initial draft

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Chicken Bacon Burger');
-INSERT INTO burgers (name) VALUES ('Cheese Burger');
-INSERT INTO burgers (name, sleepy) VALUES ('Fish Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', true);
